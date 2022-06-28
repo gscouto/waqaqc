04dbbd0867e18337858d5e677bba36f91777b41a
gscouto <guilherme.couto86@gmail.com>
2022-06-28 17:54:15 +0200

    Updating QC plots.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='waqaqc',
+     version='0.1',
+     author="Guilherme Couto",
+     author_email="",
+     description="",
+     url="",
+     packages=['waqaqc'])
