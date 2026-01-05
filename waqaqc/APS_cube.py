0317a5bf396ec6f538ef6c81afb97592dda42ff4
Guilherme Couto <guilherme.couto86@gmail.com>
2026-01-05 15:07:25 +0100

    Fixing APS_cube with new data model.

--- waqaqc/APS_cube.py
+++ waqaqc/APS_cube.py
@@ -104,10 +104,8 @@ def cube_creator(ob, args):
 
     os.makedirs(gal_dir, exist_ok=True)
 
-    breakpoint()
-
     # aps_id = c[2].data['APS_ID'] # commenting due to being dropped (c1)
-    bin_id = cube[1].data['BIN_ID']
+    # bin_id = cube[1].data['BIN_ID']
     r_bin_id = cube[2].data['BIN_ID']
 
     wave = np.exp(cube[2].data['LOGLAM'][0])
