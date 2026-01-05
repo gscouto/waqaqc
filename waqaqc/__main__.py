fa3ce6f2ac8f0d83b284684a54df4d3beb0944ff
Guilherme Couto <guilherme.couto86@gmail.com>
2026-01-05 12:38:10 +0100

    Implementing integrated runner and config file. Implementing yaml file.

--- waqaqc/__main__.py
+++ waqaqc/__main__.py
@@ -2,6 +2,7 @@ from . import run_waqaqc
 import argparse
 import sys
 import ast
+import yaml
 
 from waqaqc.config import defaults
 
@@ -9,6 +10,7 @@ from waqaqc.config import defaults
 def main():
     parser = argparse.ArgumentParser(prog='waqaqc - WEAVE-Apertif Quality Assurance (WAQA) Quality Control plots')
 
+    parser.add_argument("-c", "--config", type=str, help="YAML configuration file")
     parser.add_argument("data_path", type=str,
                         help="Full path to the main data folder")
     parser.add_argument("ob_list", type=str,
@@ -105,6 +107,13 @@ def main():
                              "factor to derived standard deviation.")
 
     args = parser.parse_args()
+
+    if args.config:
+        with open(args.config) as f:
+            cfg = yaml.safe_load(f)
+
+        for k, v in cfg.items():
+            setattr(args, k, v)
 
     ob_list = parse_csv_list(args.ob_list, "ob_list")
 
