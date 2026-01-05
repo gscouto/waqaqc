1c675b1ba031c2d9b3296dc88f522d3fb4e13f1c
Guilherme Couto <guilherme.couto86@gmail.com>
2026-01-05 14:42:27 +0100

    Fixing APS_cube with new data model.

--- waqaqc/APS_cube.py
+++ waqaqc/APS_cube.py
@@ -96,7 +96,7 @@ def cube_creator(ob, args):
     file_dir = args.data_path + ob + '/'
 
     wcs_c = fits.open(file_dir + np.sort([x for x in os.listdir(file_dir) if ('stackcube' in x)])[1])
-    c = fits.open(file_dir + np.sort([x for x in os.listdir(file_dir) if ('LWVE' in x)])[0])
+    cube = fits.open(file_dir + np.sort([x for x in os.listdir(file_dir) if ('LWVE' in x)])[0])
 
     gal_id = c[1].data['CNAME'][0]
     # gal_dir = gal_id + '_' + wcs_c[0].header['MODE'] + '_' + str(wcs_c[0].header['OBID'])
