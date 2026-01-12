94a4576b031f6046f9c17517aafd241ca7ed4113
Guilherme Couto <guilherme.couto86@gmail.com>
2026-01-12 17:41:09 +0100

    Fixing APS_cube with new data model.

--- waqaqc/QC_plots.py
+++ waqaqc/QC_plots.py
@@ -1405,8 +1405,6 @@ def html_plots(ob, redshift, args):
 
         return sn_cov
 
-    breakpoint()
-
     if args.cov_flag == 1:
         try:
             binNum, xNode, yNode, xBar, yBar, sn, nPixels, scale = voronoi_2d_binning(x_t_r, y_t_r, sgn_tt_r, rms_tt_r,
