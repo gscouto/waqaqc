bc640c06919f2be4e3936b60c062c2833475c30a
Guilherme Couto <guilherme.couto86@gmail.com>
2026-01-13 14:39:54 +0100

    Fixing APS_cube with new data model.

--- waqaqc/APS_cube.py
+++ waqaqc/APS_cube.py
@@ -61,10 +61,12 @@ def process_vorbin_pixel(idx_i, pix, vorbin_map, r_bin_id, bin_id,
 
     try:
         bin_mask = (r_bin_id == bin_val)
-        factor = bin_pixel_counts.get(bin_val, 1)  # avoid division by 0
+        # factor = bin_pixel_counts.get(bin_val, 1)  # avoid division by 0
 
-        data_slice = vorbin_cube_data[bin_mask][0] / factor
-        err_slice = vorbin_cube_err[bin_mask][0] / factor
+        # data_slice = vorbin_cube_data[bin_mask][0] / factor
+        # err_slice = vorbin_cube_err[bin_mask][0] / factor
+        data_slice = vorbin_cube_data[bin_mask][0]
+        err_slice = vorbin_cube_err[bin_mask][0]
         vel_val = data4_V[r_bin_id == bin_id[idx_i]][0]
 
         return x, y, data_slice, err_slice, vel_val
@@ -111,6 +113,8 @@ def cube_creator(ob, args):
     # aps_id = c[2].data['APS_ID'] # commenting due to being dropped (c1)
     bin_id = cube['PATCH_TABLE'].data['BIN_ID']
     r_bin_id = cube['PATCH_BINSPEC'].data['BIN_ID']
+
+    breakpoint()
 
     wave = np.exp(cube['PATCH_BINSPEC'].data['LOGLAM'][0])
     if wcs_c[0].header['MODE'] == 'HIGHRES':
