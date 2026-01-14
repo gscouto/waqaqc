1493e65ac50f13e230fe6e1a506940a37c15de6d
Guilherme Couto <guilherme.couto86@gmail.com>
2026-01-14 09:51:34 +0100

    Fixing APS_cube with new data model.

--- waqaqc/APS_cube.py
+++ waqaqc/APS_cube.py
@@ -65,8 +65,8 @@ def process_vorbin_pixel(idx_i, pix, vorbin_map, r_bin_id, bin_id,
 
         data_slice = vorbin_cube_data[bin_mask][0] / factor
         err_slice = vorbin_cube_err[bin_mask][0] / factor
-        data_slice = vorbin_cube_data[bin_mask][0]
-        err_slice = vorbin_cube_err[bin_mask][0]
+        # data_slice = vorbin_cube_data[bin_mask][0]
+        # err_slice = vorbin_cube_err[bin_mask][0]
         vel_val = data4_V[r_bin_id == bin_id[idx_i]][0]
 
         return x, y, data_slice, err_slice, vel_val
