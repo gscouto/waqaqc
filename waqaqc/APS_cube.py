daf36115bcdf6e89698fb26a66be6d367a316ba4
Guilherme Couto <guilherme.couto86@gmail.com>
2026-01-16 14:23:29 +0100

    Fixing APS_cube with new data model.

--- waqaqc/APS_cube.py
+++ waqaqc/APS_cube.py
@@ -72,7 +72,7 @@ def process_vorbin_pixel(idx_i, pix, vorbin_map, r_bin_id, bin_id,
         return x, y, data_slice, err_slice, vel_val
 
     except Exception:
-        print("Failed pixel:", x, y, "bin:", bin_val)
+        print("Failed pixel:", x, y, "bin:", bin_val, "r_bin_id:", r_bin_id)
         return None
 
 
