d5b841976b161e9cc1f25f6381ca108c12dad600
Guilherme Couto <guilherme.couto86@gmail.com>
2026-01-26 17:36:32 +0100

    Fixing APS_cube with new data model.

--- waqaqc/table_creator.py
+++ waqaqc/table_creator.py
@@ -763,30 +763,22 @@ def tab_cre(ob, args):
             ttx = tx
             tty = ty
 
-        for k in np.arange(len(stelt_file[1].data['fiber'])):
-            print(
-                'Organizing tables formats: ' + str(round(100. * k / np.nanmax(stelt_file[1].data['fiber']), 2)) + '%',
-                end='\r')
-            if args.vorbin_flag == 1:
-                for j in np.arange(len(np.where(vorbin_map == k)[0]) - 1):
-                    if args.el_flag == 1:
-                        if np.sum(elint_file[1].data['fiber'] == k) > 0:
-                            tab_el.add_row(tab_el[tab_el['fiber'] == k][0])
-                        else:
-                            new_row = tab_el[tab_el['fiber'] == 0][0]
-                            for i in np.arange(len(new_row)-1):
-                                new_row[i+1] = np.nan
-                            new_row['fiber'] = k
-                            tab_el.add_row(new_row)
-                    tab_st.add_row(tab_st[tab_st['fiber'] == k][0])
-            else:
-                tab_st.add_row(tab_st[k])
-                if args.el_flag == 1:
-                    tab_el.add_row(tab_el[k])
-
-        print('')
-
-        breakpoint()
+        if args.vorbin_flag == 1:
+            for k in np.arange(len(stelt_file[1].data['fiber'])):
+                print('Organizing tables formats: ' +
+                      str(round(100. * k / np.nanmax(stelt_file[1].data['fiber']), 2)) + '%', end='\r')
+                    for j in np.arange(len(np.where(vorbin_map == k)[0]) - 1):
+                        if args.el_flag == 1:
+                            if np.sum(elint_file[1].data['fiber'] == k) > 0:
+                                tab_el.add_row(tab_el[tab_el['fiber'] == k][0])
+                            else:
+                                new_row = tab_el[tab_el['fiber'] == 0][0]
+                                for i in np.arange(len(new_row)-1):
+                                    new_row[i+1] = np.nan
+                                new_row['fiber'] = k
+                                tab_el.add_row(new_row)
+                        tab_st.add_row(tab_st[tab_st['fiber'] == k][0])
+            print('')
 
         tab_st = tab_st[tab_st.argsort(['fiber'])]
 
