30e9bce2b50bb80ad1ae6d77732985b850b26fcc
Guilherme Couto <guilherme.couto86@gmail.com>
2026-04-02 12:13:07 +0200

    Fixing the SNR to calculate properly in per A

--- waqaqc/QC_plots.py
+++ waqaqc/QC_plots.py
@@ -953,10 +953,10 @@ def html_plots(ob, redshift, args):
                    color=single_file[1].name[:-5], edgecolor='black')
         ax.scatter(mag_obs, snr_obs/snr_band_etc, s=20, marker='o', alpha=0.3,
                    color=single_file[1].name[:-5], edgecolor='black')
-        # ax.axhline(1, color='black', linestyle='--', linewidth=1)
+        ax.axhline(1, color='black', linestyle='--', linewidth=1)
         ax.set_xlim([13, 26])
         # ax.set_ylim([0., 1.5])
-        ax.set_ylim([0.5, 1.5])
+        ax.set_ylim([0., 2.0])
         ax.set_xlabel(band + ' band mag (Vega)')
         # ax.set_ylabel(r'S/N ratio (|$\Delta$ SNR| / SNR_ETC) [per $\AA$]')
         ax.set_ylabel(r'S/N ratio (SNR_obs / SNR_ETC) [per $\AA$]')
