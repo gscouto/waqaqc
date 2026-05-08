b1df52d86d902c7539a3eae988db274281d4dc45
Guilherme Couto <guilherme.couto86@gmail.com>
2026-05-08 20:46:49 +0200

    Trying to optimize QC_plots

--- waqaqc/QC_plots.py
+++ waqaqc/QC_plots.py
@@ -28,61 +28,6 @@ except ImportError:
     from importlib_resources import files  # Backport for Python 3.8
 
 
-def getimages(ra, dec, filters="grizy"):
-    """Query ps1filenames.py service to get a list of images
-
-    ra, dec = position in degrees
-    size = image size in pixels (0.25 arcsec/pixel)
-    filters = string with filters to include
-    Returns a table with the results
-    """
-
-    service = "https://ps1images.stsci.edu/cgi-bin/ps1filenames.py"
-    url = f"{service}?ra={ra}&dec={dec}&filters={filters}"
-    table = Table.read(url, format='ascii')
-    return table
-
-
-def geturl(ra, dec, size=240, output_size=None, filters="grizy", im_format="jpg", color=False):
-    """Get URL for images in the table
-
-    ra, dec = position in degrees
-    size = extracted image size in pixels (0.25 arcsec/pixel)
-    output_size = output (display) image size in pixels (default = size).
-                  output_size has no effect for fits format images.
-    filters = string with filters to include
-    im_format = data format (options are "jpg", "png" or "fits")
-    color = if True, creates a color image (only for jpg or png format).
-            Default is return a list of URLs for single-filter grayscale images.
-    Returns a string with the URL
-    """
-
-    if color and im_format == "fits":
-        raise ValueError("color images are available only for jpg or png formats")
-    if im_format not in ("jpg", "png", "fits"):
-        raise ValueError("im_format must be one of jpg, png, fits")
-    table = getimages(ra, dec, filters=filters)
-    url = (f"https://ps1images.stsci.edu/cgi-bin/fitscut.cgi?"
-           f"ra={ra}&dec={dec}&size={size}&format={im_format}")
-    if output_size:
-        url = url + "&output_size={}".format(output_size)
-    # sort filters from red to blue
-    flist = ["yzirg".find(x) for x in table['filter']]
-    table = table[np.argsort(flist)]
-    if color:
-        if len(table) > 3:
-            # pick 3 filters
-            table = table[[0, len(table) // 2, len(table) - 1]]
-        for i, param in enumerate(["red", "green", "blue"]):
-            url = url + "&{}={}".format(param, table['filename'][i])
-    else:
-        urlbase = url + "&red="
-        url = []
-        for filename in table['filename']:
-            url.append(urlbase + filename)
-    return url
-
-
 def vorbin_loop(args):
     i, vorbin_m, cam = args
 
