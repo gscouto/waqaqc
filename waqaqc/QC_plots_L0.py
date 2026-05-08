74b4e443e82c744beb8c2d7039511b68c8d6eaba
Guilherme Couto <guilherme.couto86@gmail.com>
2026-05-08 21:31:39 +0200

    Trying to optimize QC_plots

--- waqaqc/QC_plots_L0.py
+++ waqaqc/QC_plots_L0.py
@@ -43,6 +43,9 @@ def fiber_lines(args):
         w_lam = lamp_lam[lam_wind_c - lam_wind: lam_wind_c + lam_wind]
         w_spec = lamp_spec[fiber][lam_wind_c - lam_wind: lam_wind_c + lam_wind]
 
+        print(i)
+        breakpoint()
+
         popt, pcov = curve_fit(gauss, w_lam, w_spec, p0=[0, 0, max(w_spec), cen_lam[i], 1],
                                bounds=([-np.inf, -np.inf, 0, 0, 0],
                                        [np.inf, np.inf, np.inf, np.inf, np.inf]))
