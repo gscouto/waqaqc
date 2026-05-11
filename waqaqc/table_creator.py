25580b18953077da962e47d6e950c2063dc9bc9a
Guilherme Couto <guilherme.couto86@gmail.com>
2026-05-11 12:28:40 +0200

    Optimizing spec_fit, and adding a cube mode fit

--- waqaqc/table_creator.py
+++ waqaqc/table_creator.py
@@ -879,8 +879,6 @@ def tab_cre(ob, args):
                     tab_st.add_row(tab_st[tab_st['fiber'] == k][0])
             print('')
 
-        breakpoint()
-
         tab_st = tab_st[tab_st.argsort(['fiber'])]
 
         tab_st.add_column(ttx, name='x_cor', index=0)
