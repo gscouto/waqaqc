9c2e585ad3f925b4d0e6bf247656c49a45e711a3
Guilherme Couto <guilherme.couto86@gmail.com>
2026-05-12 19:20:47 +0200

    Optimizing spec_fit, and adding a cube mode fit

--- waqaqc/pyp_params.py
+++ waqaqc/pyp_params.py
@@ -310,10 +310,10 @@ def pp(ob, redshift, args):
         f.write(
             'nwidth_norm        150                                         '
             '!Width of running mean in pixels for the normalization (int)\n')
-        f.write('start_wave         ' + args.red_lam_min +
+        f.write('start_wave         ' + str(args.red_lam_min) +
                 '                                   !Lower wavelength limit for the stellar population fitting\n')
         start_wave = args.red_lam_min
-        f.write('end_wave           ' + args.red_lam_max +
+        f.write('end_wave           ' + str(args.red_lam_max) +
                 '                                   !Upper wavelength limit for the stellar population fitting\n')
         end_wave = args.red_lam_max
         f.write('min_x              1                                           !Minimum x dimension (int)\n')
