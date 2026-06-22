710af549f25c0e4a736443f5d259911cc9f9ca13
Guilherme Couto <guilherme.couto86@gmail.com>
2026-06-22 10:35:39 +0200

    Adding a flux calibration plot.

--- waqaqc/spec_fit.py
+++ waqaqc/spec_fit.py
@@ -49,20 +49,20 @@ def create_rss_from_cube(cube, vorbin_map=None,
             sens = cube[5].data
 
             rss_data[k] = cube[1].data[:, y, x]
-            # rss_err[k] = cube[2].data[:, y, x]
+            rss_err[k] = cube[2].data[:, y, x]
 
             rss_data[k] *= sens
-            # rss_err[k] *= sens
+            rss_err[k] *= sens
 
-            ivar = cube[2].data[:, y, x]
-
-            ivar /= sens ** 2
-
-            rss_err[k] = np.where(
-                ivar > 0,
-                1.0 / np.sqrt(ivar),
-                np.nan
-            )
+            # ivar = cube[2].data[:, y, x]
+            #
+            # ivar /= sens ** 2
+            #
+            # rss_err[k] = np.where(
+            #     ivar > 0,
+            #     1.0 / np.sqrt(ivar),
+            #     np.nan
+            # )
 
             rss_data *= 1e20
             rss_err *= 1e20
@@ -195,22 +195,22 @@ def run_mode(mode, ob, args, gal, gal_dir, file_dir, stackcubes):
             cube = fits.open(gal_dir + '/' + cfg['cube_file'])
 
         flux = cube[1].data.copy()
-        # err = cube[2].data.copy()
-        ivar = cube[2].data.copy()
+        err = cube[2].data.copy()
+        # ivar = cube[2].data.copy()
 
         if cfg['scale_flux']:
             sens = cube[5].data
 
             flux *= sens[:, None, None]
-            # err *= sens[:, None, None]
+            err *= sens[:, None, None]
 
-            ivar /= sens[:, None, None] ** 2
-
-            err = np.where(
-                ivar > 0,
-                1.0 / np.sqrt(ivar),
-                np.nan
-            )
+            # ivar /= sens[:, None, None] ** 2
+            #
+            # err = np.where(
+            #     ivar > 0,
+            #     1.0 / np.sqrt(ivar),
+            #     np.nan
+            # )
             flux *= 1e20
             err *= 1e20
 
