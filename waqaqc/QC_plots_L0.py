88575ad25739f3b8b5beb82afc5b628bd31ceada
Guilherme Couto <guilherme.couto86@gmail.com>
2026-07-28 17:21:33 +0200

    Trying to improve in the NED timeout crashes.

--- waqaqc/QC_plots_L0.py
+++ waqaqc/QC_plots_L0.py
@@ -931,8 +931,9 @@ def plots(blue_cube, file_dir, gal_dir, file_list, warc_list, output_str, redshi
         cb = plt.colorbar(sc, ax=ax)
         cb.set_label('g mag')
 
-        median_flux_calib = 100 * (1 - (np.nanmean([abs(delta_gr_median), abs(delta_ri_median), abs(delta_gi_median)])/0.5))
-        std_flux_calib = 100 * (1 - np.nanmean([delta_gr_std, delta_ri_std, delta_gi_std])) / 0.75
+        median_flux_calib = 100 * (1 - (np.nanmean([abs(delta_gr_median), abs(delta_ri_median),
+                                                    abs(delta_gi_median)])/0.5))
+        std_flux_calib = 100 * (1 - np.nanmean([delta_gr_std, delta_ri_std, delta_gi_std]))
 
         if median_flux_calib < 0:
             median_flux_calib = 0
